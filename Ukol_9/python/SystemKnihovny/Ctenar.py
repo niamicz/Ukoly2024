cf732e6de21086f9ac5672b0ceef8122c099527b
niamicz <167056957+niamicz@users.noreply.github.com>
2025-01-06 17:06:34 +0100

    Update Ctenar.py

--- Ukol_9/python/SystemKnihovny/Ctenar.py
+++ Ukol_9/python/SystemKnihovny/Ctenar.py
@@ -1,17 +1,26 @@
-import random
-
-
 class Ctenar:
-    def __init__(self, jmeno: str, prijmeni: str):
+    def __init__(self, jmeno, prijmeni):
         self._jmeno = jmeno
         self._prijmeni = prijmeni
-        self.cislo_prukazky = self.vygeneruj_cislo_prukazky()
+        self._cislo_prukazky = self._vygeneruj_cislo_prukazky()
 
-    # doplňte potřebné gettry a settry
+    @property
+    def jmeno(self):
+        return self._jmeno
 
-    @staticmethod
-    def vygeneruj_cislo_prukazky() -> int:
-        pass
+    @property
+    def prijmeni(self):
+        return self._prijmeni
 
-    def __str__(self) -> str:
-        return ""
+    @property
+    def cislo_prukazky(self):
+        return self._cislo_prukazky
+
+    @cislo_prukazky.setter
+    def cislo_prukazky(self, value):
+        if value <= 0:
+            raise ValueError("Číslo průkazky musí být kladné")
+        self._cislo_prukazky = value
+
+    def _vygeneruj_cislo_prukazky(self):
+        return 1  # Příklad generování čísla průkazky
