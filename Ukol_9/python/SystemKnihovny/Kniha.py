1f2196d9276a9e61c9609372c763b230df35cb4c
niamicz <167056957+niamicz@users.noreply.github.com>
2025-01-06 17:06:56 +0100

    Update Kniha.py

--- Ukol_9/python/SystemKnihovny/Kniha.py
+++ Ukol_9/python/SystemKnihovny/Kniha.py
@@ -1,11 +1,31 @@
 class Kniha:
-    def __init__(self, nazev: str, autor: str, rok_vydani: int, isbn: str):
+    def __init__(self, nazev, autor, rok_vydani, isbn):
         self._nazev = nazev
         self._autor = autor
         self._rok_vydani = rok_vydani
         self.isbn = isbn
 
-    # doplňte potřebné gettry a setry
+    @property
+    def nazev(self):
+        return self._nazev
 
-    def __str__(self) -> str:
-        return ""
+    @property
+    def autor(self):
+        return self._autor
+
+    @property
+    def rok_vydani(self):
+        return self._rok_vydani
+
+    @property
+    def isbn(self):
+        return self._isbn
+
+    @isbn.setter
+    def isbn(self, value):
+        if not self._validuj_isbn(value):
+            raise ValueError("Neplatné ISBN")
+        self._isbn = value
+
+    def _validuj_isbn(self, isbn):
+        return len(isbn) in (10, 13) and isbn.isdigit()
